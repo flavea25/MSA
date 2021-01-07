39f8b53f5cfb264130b682a714103667704df454
Lazin Alexandra <lazinalexandra98@gmail.com>
2021-01-07 14:55:17 +0200

    Profile page with info
    Signed-off-by: Lazin Alexandra <lazinalexandra98@gmail.com>

--- app/src/main/java/com/example/lostfoundpets/ui/profile/LoggedProfileFragment.java
+++ app/src/main/java/com/example/lostfoundpets/ui/profile/LoggedProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.lostfoundpets.ui.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +16,28 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.lostfoundpets.MainActivity;
 import com.example.lostfoundpets.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import javax.annotation.Nullable;
+
 
 public class LoggedProfileFragment extends Fragment {
+    private static final int GALLERY_INTENT_CODE = 1023 ;
+    private final CollectionReference users = FirebaseFirestore.getInstance().collection("users");
+    TextView fullName,email,phone;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    FirebaseUser user;
+    StorageReference storageReference;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,9 +50,34 @@ public class LoggedProfileFragment extends Fragment {
         logoutButton.setOnClickListener(v -> {
             fAuth.signOut();
             startActivity(new Intent(getContext(), MainActivity.class));
-//            logoutButton.setVisibility(View.GONE);
-//            textView.setVisibility(View.GONE);
         });
+
+            phone = root.findViewById(R.id.profileNumber);
+            fullName = root.findViewById(R.id.profileName);
+            email    = root.findViewById(R.id.profileEmail);
+
+
+            fAuth = FirebaseAuth.getInstance();
+            fStore = FirebaseFirestore.getInstance();
+            storageReference = FirebaseStorage.getInstance().getReference();
+
+            userId = fAuth.getCurrentUser().getUid();
+            user = fAuth.getCurrentUser();
+            DocumentReference documentReference = fStore.collection("users").document(userId);
+                documentReference.addSnapshotListener( new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                    if(documentSnapshot.exists()){
+                        phone.setText(documentSnapshot.getString("phoneNumber"));
+                        fullName.setText(documentSnapshot.getString("fullName"));
+                        email.setText(documentSnapshot.getString("email"));
+
+                    }else {
+                        Log.d("tag", "onEvent: Document do not exists");
+                    }
+                }
+            });
+
         return root;
     }
 }
